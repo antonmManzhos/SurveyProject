7f34bd811814413679c2a431d5fe43e8d6b14327
Anton Manzhos <antonm1.manzhos@gmail.com>
2025-11-24 20:21:13 +0200

    added mssql support

--- SurveyProject/settings.py
+++ SurveyProject/settings.py
@@ -75,9 +75,21 @@ WSGI_APPLICATION = 'SurveyProject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'MySurveyDB',
+        'USER': 'sa',
+        'PASSWORD': '123456',
+        'HOST': 'DESKTOP-88OSCIO\SQLEXPRESS',
+        'PORT': '1433',
+
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        }
     }
 }
 
